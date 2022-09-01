10318397d9dd3e9d2cfb25de681f4efc1c858d9e
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-08-31 22:00:03 -0300

    Adiciona requisito 3 e 4

--- script.js
+++ script.js
@@ -2,37 +2,39 @@ const color1 = document.getElementById('firstColor');
 const color2 = document.getElementById('secondColor');
 const color3 = document.getElementById('thirdColor');
 const color4 = document.getElementById('fourthColor');
+const buttonColors = document.getElementById('button-random-color');
 
-function generateRandomColors() {
+function generateRandomColor() {
   const red = Math.floor(Math.random() * 255);
   const green = Math.floor(Math.random() * 255);
   const blue = Math.floor(Math.random() * 255);
 
   if (red === 255 && green === 255 && blue === 255) {
-    generateRandomColors();
+    generateRandomColor();
   }
 
   const colorRandom = `rgb(${red}, ${green}, ${blue})`;
   return colorRandom;
 }
 
-// eslint-disable-next-line complexity
-function completePalette() {
-  color1.style.backgroundColor = 'black';
-  color2.style.backgroundColor = generateRandomColors();
-  color3.style.backgroundColor = generateRandomColors();
-  color4.style.backgroundColor = generateRandomColors();
-
-  if (color2 === color1 || color2 === color3 || color2 === color4) {
-    color2.style.backgroundColor = generateRandomColors();
+function generateDistinctColors(count) {
+  const colors = [];
+  while (colors.length < count) {
+    const color = generateRandomColor();
+    if (!colors.includes(color)) {
+      colors.push(color);
+    }
   }
-
-  if (color3 === color1 || color3 === color2 || color3 === color4) {
-    color3.style.backgroundColor = generateRandomColors();
-  }
-  if (color4 === color1 || color4 === color2 || color4 === color3) {
-    color4.style.backgroundColor = generateRandomColors();
-  }
+  return colors;
 }
 
+function completePalette() {
+  const [firstColor, secondColor, thirdColor] = generateDistinctColors(3);
+  color1.style.backgroundColor = 'rgb(0, 0, 0)';
+  color2.style.backgroundColor = firstColor;
+  color3.style.backgroundColor = secondColor;
+  color4.style.backgroundColor = thirdColor;
+}
 completePalette();
+
+buttonColors.addEventListener('click', completePalette);
