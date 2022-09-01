f6cd1a7b8e61838daca5eb1dbbb88d22950b6a40
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-08-31 21:35:25 -0300

    Reiniciando o projeto, adicionando requisito 1 e 2

--- script.js
+++ script.js
@@ -1,70 +1,38 @@
-const color1 = document.getElementById('black');
+const color1 = document.getElementById('firstColor');
 const color2 = document.getElementById('secondColor');
 const color3 = document.getElementById('thirdColor');
 const color4 = document.getElementById('fourthColor');
-const containerBoard = document.querySelector('#pixel-board');
 
-function onClickPixel() {
-  const colorSelected = document.querySelector('.selected');
-  const color = colorSelected.style.backgroundColor;
-  this.style.backgroundColor = color;
-}
-
-function onClickPalletColor() {
-  const prevSelected = document.querySelector('.selected');
-  prevSelected.classList.remove('selected');
-  this.classList.add('selected');
-}
-
-function createBoardPixels(size) {
-  const lenghtBoard = size * size;
-  containerBoard.innerHTML = '';
-  containerBoard.style.width = `${size * 42}px`;
-  for (let index = 0; index < lenghtBoard; index += 1) {
-    const pixel = document.createElement('div');
-    pixel.className = 'pixel';
-    containerBoard.appendChild(pixel);
-
-    pixel.addEventListener('click', onClickPixel);
-  }
-}
-
-function randomColorGenerator() {
+function generateRandomColors() {
   const red = Math.floor(Math.random() * 255);
   const green = Math.floor(Math.random() * 255);
   const blue = Math.floor(Math.random() * 255);
 
   if (red === 255 && green === 255 && blue === 255) {
-    randomColorGenerator();
+    generateRandomColors();
   }
 
-  const colorRGB = `rgb(${red}, ${green}, ${blue})`;
-  console.log(colorRGB);
-
-  return colorRGB;
+  const colorRandom = `rgb(${red}, ${green}, ${blue})`;
+  return colorRandom;
 }
 
-function createColorPalette() {
-  color1.style.backgroundColor = 'rgb(0, 0, 0)';
-  color2.style.backgroundColor = randomColorGenerator();
-  color3.style.backgroundColor = randomColorGenerator();
-  color4.style.backgroundColor = randomColorGenerator();
+// eslint-disable-next-line complexity
+function completePalette() {
+  color1.style.backgroundColor = 'black';
+  color2.style.backgroundColor = generateRandomColors();
+  color3.style.backgroundColor = generateRandomColors();
+  color4.style.backgroundColor = generateRandomColors();
+
+  if (color2 === color1 || color2 === color3 || color2 === color4) {
+    color2.style.backgroundColor = generateRandomColors();
+  }
+
+  if (color3 === color1 || color3 === color2 || color3 === color4) {
+    color3.style.backgroundColor = generateRandomColors();
+  }
+  if (color4 === color1 || color4 === color2 || color4 === color3) {
+    color4.style.backgroundColor = generateRandomColors();
+  }
 }
 
-const inputSize = document.getElementById('board-size');
-inputSize.addEventListener('change', () => {
-  const size = inputSize.value;
-  createBoardPixels(size);
-});
-
-color1.addEventListener('click', onClickPalletColor);
-color2.addEventListener('click', onClickPalletColor);
-color3.addEventListener('click', onClickPalletColor);
-color4.addEventListener('click', onClickPalletColor);
-
-createColorPalette();
-
-const buttonGeneratorColor = document.getElementById('button-random-color');
-buttonGeneratorColor.addEventListener('click', createColorPalette);
-
-createBoardPixels(5);
+completePalette();
