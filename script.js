a90f5a10f172e28d31e3623fcc19196a12442e15
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-09-01 22:20:27 -0300

    adiciona requisito 12 e 15

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const localStorageKey = 'colorPalette';
+const colorKey = 'colorPalette';
+const pixelBoardKey = 'pixelBoard';
+const boardSizeKey = 'boardSize';
+const defaultColor = 'rgb(255,255,255)';
 const color1 = document.getElementById('firstColor');
 const color2 = document.getElementById('secondColor');
 const color3 = document.getElementById('thirdColor');
@@ -22,6 +25,25 @@ function generateRandomColor() {
   return colorRandom;
 }
 
+function getLocalStorage(key) {
+  const value = localStorage.getItem(key);
+  if (!value) {
+    return;
+  }
+  return JSON.parse(value);
+}
+
+function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+function onChangeBoardSize(size, cleanBoardColors) {
+  setLocalStorage(boardSizeKey, size);
+  if (cleanBoardColors) {
+    setLocalStorage(pixelBoardKey, Array(size * size).fill(defaultColor));
+  }
+}
+
 function generateDistinctColors(count) {
   const colors = [];
   while (colors.length < count) {
@@ -41,28 +63,39 @@ function completePalette(colors) {
   color4.style.backgroundColor = thirdColor;
 }
 
-function saveColors(colors) {
-  localStorage.setItem(localStorageKey, JSON.stringify(colors));
+function fillSavedColors() {
+  const pixels = getLocalStorage(pixelBoardKey);
+  if (!pixels) {
+    return;
+  }
+
+  const children = Array.from(containerBoard.children);
+  for (let i = 0; i < children.length; i += 1) {
+    const pixel = children[i];
+    const color = pixels[i];
+    pixel.style.backgroundColor = color;
+  }
+}
+
+function getElementIndex(element) {
+  return Array.from(element.parentNode.children).indexOf(element);
 }
 
 function onClickGenerateColors() {
   const colors = generateDistinctColors(3);
   completePalette(colors);
-  saveColors(colors);
-}
-
-function getSavedColors() {
-  const value = localStorage.getItem(localStorageKey);
-  if (!value) {
-    return;
-  }
-  return JSON.parse(value);
+  setLocalStorage(colorKey, colors);
 }
 
 function onClickPixel() {
   const colorSelected = document.querySelector('.selected');
   const color = colorSelected.style.backgroundColor;
   this.style.backgroundColor = color;
+
+  const index = getElementIndex(this);
+  const pixels = getLocalStorage(pixelBoardKey);
+  pixels[index] = color;
+  setLocalStorage(pixelBoardKey, pixels);
 }
 
 function onClickPaletteColor() {
@@ -71,15 +104,7 @@ function onClickPaletteColor() {
   this.classList.add('selected');
 }
 
-function onClickClear() {
-  const pixel = document.getElementsByClassName('pixel');
-  for (let index = 0; index < pixel.length; index += 1) {
-    pixel[index].style.backgroundColor = 'rgb(255, 255, 255)';
-  }
-  return pixel;
-}
-
-function createBoardPixels(size) {
+function createBoardPixels(size, cleanBoardColors) {
   const lenghtBoard = size * size;
   containerBoard.innerHTML = '';
   containerBoard.style.width = `${size * 42}px`;
@@ -90,6 +115,43 @@ function createBoardPixels(size) {
 
     pixel.addEventListener('click', onClickPixel);
   }
+  onChangeBoardSize(size, cleanBoardColors);
+}
+
+function onClickClear() {
+  const boardSize = getLocalStorage(boardSizeKey) || 5;
+  createBoardPixels(boardSize, true);
+}
+
+function onLoadPage() {
+  const boardSize = getLocalStorage(boardSizeKey) || 5;
+  let colors = getLocalStorage(colorKey);
+  const cleanBoardColors = !colors;
+
+  if (!colors) {
+    colors = generateDistinctColors(3);
+  }
+
+  createBoardPixels(boardSize, cleanBoardColors);
+  completePalette(colors);
+
+  fillSavedColors();
+
+  setLocalStorage(colorKey, colors);
+  setLocalStorage(boardSizeKey, boardSize);
+}
+
+function clamp(number, min, max) {
+  return Math.max(min, Math.min(number, max));
+}
+function onClickGenerateBoard() {
+  let size = parseInt(input.value, 10);
+  if (!size) {
+    alert('Board Inválido!');
+    size = 5;
+  }
+  size = clamp(size, 5, 50);
+  createBoardPixels(size, true);
 }
 
 buttonColors.addEventListener('click', onClickGenerateColors);
@@ -98,29 +160,6 @@ color1.addEventListener('click', onClickPaletteColor);
 color2.addEventListener('click', onClickPaletteColor);
 color3.addEventListener('click', onClickPaletteColor);
 color4.addEventListener('click', onClickPaletteColor);
-generateBoard.addEventListener('click', () => {
-  let size = input.value;
-  if (size === '') {
-    alert('Board Inválido!');
-    size = 5;
-  }
-  if (size < 5) {
-    size = 5;
-  }
-  if (size > 50) {
-    size = 50;
-  }
-  createBoardPixels(size);
-});
+generateBoard.addEventListener('click', onClickGenerateBoard);
 
-window.onload = () => {
-  const colorsSaved = getSavedColors();
-  createBoardPixels(5);
-  if (colorsSaved) {
-    completePalette(colorsSaved);
-    return;
-  }
-  const colors = generateDistinctColors(3);
-  completePalette(colors);
-  saveColors(colors);
-};
+window.onload = onLoadPage;
